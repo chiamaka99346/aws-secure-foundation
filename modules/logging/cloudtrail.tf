5986ac17ff5af255c92b95f773e61a83aa49f8ac
chiamaka <amakadums1990@gmail.com>
2025-11-11 10:42:37 +0100

    fix: add CloudTrail encryption and log validation, enable S3 logs bucket encryption

--- modules/logging/cloudtrail.tf
+++ modules/logging/cloudtrail.tf
@@ -13,6 +13,17 @@ resource "aws_s3_bucket" "logs" {
   }
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+    bucket_key_enabled = true
+  }
+}
+
 resource "aws_s3_bucket_versioning" "logs" {
   bucket = aws_s3_bucket.logs.id
 
@@ -69,6 +80,7 @@ resource "aws_cloudtrail" "main" {
   include_global_service_events = true
   is_multi_region_trail         = true
   enable_logging                = true
+  enable_log_file_validation    = true
 
   tags = {
     Name = "cloudtrail-${var.org_env}"
